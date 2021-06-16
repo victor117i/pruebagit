67f7b68a696fc30cb73624b674b77cdbbe4d2748
victor <vmosquedarosales@gmail.com>
2021-06-16 08:08:50 -0500

    CINE

---
+++ CINE.php
@@ -0,0 +1,49 @@
+<?php
+class CINES {
+
+    public $CINE;
+    public $SALA;
+    public $CLIENTE;
+
+    function inscribir($cine,$sala){
+        return $cliente;
+        }
+  }
+
+  class CINE {
+
+    public $numero_cine;
+    public $nombre;
+    public $ubicacion;
+    
+
+    function unidad($nombre,$ubicacion){
+      return $numero_cine;
+    }
+  }
+
+  class Sala{
+
+    public $potencia_maxima;
+    public $asientos;
+  }
+
+  class Pelicula {
+
+    public $nombre;
+    public $Clasificacion;
+    public $Precio;
+
+    function disponibilidad($nombre, $Clasificacion, $Precio){
+        $this->Nombre=$nombre;
+        $this->Edad=$Clasificacion;
+        $this->Costo=$Numero_seguro_social;
+        return $this->nombre;
+        }
+
+    function  no_disponible($clave){
+        return;
+    }
+  }
+
+?>
